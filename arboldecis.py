000857bd3b05e19f6e8e87afaaf55fa942aff613
mcknago <74916664+mcknago@users.noreply.github.com>
2022-05-24 13:46:47 -0500

    Update arboldecis.py

--- arboldecis.py
+++ arboldecis.py
@@ -4,7 +4,6 @@ import random
 class arbolDecis:
     def tomar_decision(self,ESTADO,BATT,SERVICIO):
         time_sleep=10
-        print(f'Arbol:El Servicio es {SERVICIO} Potencia PROMEDIO es: {BATT}')
         if ESTADO==1:
             state=int(random.randint(1,4))
         elif ESTADO==2:
@@ -17,5 +16,4 @@ class arbolDecis:
             state=int(random.randint(1,2))
             if state==2:
                 state=4
-        print(f'Arbol: El estado es {state} y el dormiré {time_sleep} segundos')
         return state,time_sleep
